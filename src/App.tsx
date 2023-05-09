626a83fdf373d15144fa341fa5bdfb0514f2e22d
Ikechukwu Nwachukwu <104449541+IkenNwa@users.noreply.github.com>
2023-05-09 10:29:00 +0100

    Created Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Login from "./views/Login"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>React Typescript Webpack Starter Template</h1>
+      <Login />
     </div>
   )
 }
---
+++ src/views/Login.tsx
@@ -0,0 +1,26 @@
+function Login() {
+  return (
+    <div className="loginPage">
+      <div className="logo">
+        <img src="/Chatter2Logo2.svg" alt="Logo" />
+        <p>Chatter</p>
+      </div>
+      <div className="loginContainer">
+        <form className="formContainer">
+          <input type="email" placeholder="E-Mail" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+        <div className="divider">
+          <hr /> OR <hr />
+        </div>
+        <div className="otherLogin">
+          <div className="item">Google</div>
+          <div className="item">Facebook</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
